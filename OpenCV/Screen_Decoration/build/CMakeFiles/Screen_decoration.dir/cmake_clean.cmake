b3f156acae77d4af8117a808717609298784810d
apachore <av_pachore@yahoo.com>
2017-02-25 20:52:18 -0800

    Video Projection-GPU Version

---
+++ OpenCV/Screen_Decoration/build/CMakeFiles/Screen_decoration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Screen_decoration.dir/screen_deco.cpp.o"
+  "Screen_decoration.pdb"
+  "Screen_decoration"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Screen_decoration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
