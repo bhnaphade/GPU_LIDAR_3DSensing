4068564502698fd3ba438677b3faad345c3b4b47
Naphade01 <bhupendra.naphade@gmail.com>
2017-04-09 11:41:30 -0700

    added main file for the photomicro sensor

---
+++ PhotoMicroSensor/main.cpp
@@ -0,0 +1,24 @@
+#include "tasks.hpp"
+#include "examples/examples.hpp"
+#include "myeint.hpp"
+/**
+ * The main() creates tasks or "threads".  See the documentation of scheduler_task class at scheduler_task.hpp
+ * for details.  There is a very simple example towards the beginning of this class's declaration.
+ *
+ * @warning SPI #1 bus usage notes (interfaced to SD & Flash):
+ *      - You can read/write files from multiple tasks because it automatically goes through SPI semaphore.
+ *      - If you are going to use the SPI Bus in a FreeRTOS task, you need to use the API at L4_IO/fat/spi_sem.h
+ *
+ * @warning SPI #0 usage notes (Nordic wireless)
+ *      - This bus is more tricky to use because if FreeRTOS is not running, the RIT interrupt may use the bus.
+ *      - If FreeRTOS is running, then wireless task may use it.
+ *        In either case, you should avoid using this bus or interfacing to external components because
+ *        there is no semaphore configured for this bus and it should be used exclusively by nordic wireless.
+ */
+int main(void)
+{
+
+	scheduler_add_task(new irq_task  (PRIORITY_LOW));
+    scheduler_start(); ///< This shouldn't return
+    return -1;
+}
