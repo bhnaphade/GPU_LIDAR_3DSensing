1bc1348395796c1902e3d22d308b5705b3f93f0d
apachore <apachore@gmail.com>
2017-05-02 20:02:21 -0700

    Video and LIDAR Data projection Merge

---
+++ OpenCV_CUDA/Video_LIDAR_Projection/build/CMakeFiles/cv_Video_LIDAR_Projection_GPU.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cv_Video_LIDAR_Projection_GPU.dir/Video_LIDAR_Projection_GPU.cpp.o"
+  "cv_Video_LIDAR_Projection_GPU.pdb"
+  "cv_Video_LIDAR_Projection_GPU"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cv_Video_LIDAR_Projection_GPU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
