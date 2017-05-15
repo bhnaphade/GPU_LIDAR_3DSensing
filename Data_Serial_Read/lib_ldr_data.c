d7449d4c5716c7b1233115d1b79f63b840b68024
Naphade01 <bhupendra.naphade@gmail.com>
2017-05-15 09:34:02 -0700

    Formatted  lib_ldr_data.c

--- Data_Serial_Read/lib_ldr_data.c
+++ Data_Serial_Read/lib_ldr_data.c
@@ -1,22 +1,26 @@
-  //https://chrisheydrick.com/2012/06/17/how-to-read-serial-data-from-an-arduino-in-linux-with-c-part-3/
+/********************************************************************
+* Program Name : lib_ldr_data.c                    
+* Description  : Read Lidar data from serial port     
+* Author       : Bhupendra Naphade                                       
+* Date         : May 01, 2017                                       
+* Notes        : This library will invoke a start command to the slave deivce to get the reading form the sensors	
+* Reference     : https://chrisheydrick.com/2012/06/17/how-to-read-serial-data-from-an-arduino-in-linux-with-c-part-3/
+*********************************************************************/
   
-      #include <sys/types.h>
-      #include <sys/stat.h>
-      #include <fcntl.h>
-      #include <termios.h>
-      #include <stdio.h>
-      #include <stdlib.h>
-        #include <string.h> 
-        #include <unistd.h>  /* UNIX Standard Definitions 	   */ 
-     #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+#include <unistd.h>  /* UNIX Standard Definitions 	   */ 
+#include <string.h>
 #include "lib_ldr_data.h"
 
-
-
-  #define readSize 8
+#define readSize 8
 
 unsigned char buf[readSize];
-
 
 struct senData get_lidar_data();
 
@@ -77,29 +81,22 @@ struct senData get_lidar_data()
  //usleep(1000*1000);
  char start ='S';
 
-   tcflush(fd, TCIFLUSH);
+  tcflush(fd, TCIFLUSH);
   write(fd, &start, 1);
   usleep(1);
 
- struct  senData sD;
-// while(1){
- /* Flush anything already in the serial buffer */
-    tcflush(fd, TCIFLUSH);
-    read(fd, &buf, readSize);
-
-     sD.S0 = GET_DATA(buf[0],buf[1]);
-     sD.S1 = GET_DATA(buf[2],buf[3]);
-     sD.S2 = GET_DATA(buf[4],buf[5]);
-     sD.S3 = GET_DATA(buf[6],buf[7]);
-        
-    /*     senData1 =(buf[0] << 8);
- senData1 |= buf[1]; */     
-   // printf("\nbytesRead=%d SenData%d = %d hex[0]=%2x hex[1]=%2x",n,i,senData1[i],buf[j],buf[j+1]); 
-  //  printf("\nData bytes %d S0 %d S1 %d S2 %d S3 %d",n,  sD.S0,sD.S1,sD.S2,sD.S3);
+  struct  senData sD;
   
-/* print how many bytes read */
- //printf("\n%d %d",buf[0]-'0',buf[1]-'0');
-//}
-close(fd);
-return sD;
+ /* Flush anything already in the serial buffer */
+  tcflush(fd, TCIFLUSH);
+  read(fd, &buf, readSize);
+
+  /*Store LIDAr rediands from buffer */
+  sD.S0 = GET_DATA(buf[0],buf[1]);
+  sD.S1 = GET_DATA(buf[2],buf[3]);
+  sD.S2 = GET_DATA(buf[4],buf[5]);
+  sD.S3 = GET_DATA(buf[6],buf[7]);
+
+  close(fd);
+  return sD;
 }
